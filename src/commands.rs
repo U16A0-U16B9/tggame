7caa33da58930a15b92e329f97cecb592dd85fd6
U16A0-U16B9 <xxvirginslayer420xx@gmail.com>
2023-04-22 21:44:15 +0200

    feat: added games

--- src/commands.rs
+++ src/commands.rs
@@ -2,14 +2,17 @@ use log::debug;
 use teloxide::{Bot, utils::command::BotCommands};
 use teloxide::prelude::*;
 use crate::game::player::register;
+use crate::game::new_game;
 
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase", description = "These commands are supported:")]
 pub enum Command {
     #[command(description = "display this text.")]
     Help,
-    #[command(description = "handle a username.")]
+    #[command(description = "register for new accounts")]
     Register,
+    #[command(description = "Create New Game")]
+    NewGame,
 }
 
 pub async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
@@ -19,6 +22,12 @@ pub async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> 
             let user = msg.from().expect("User not found");
             let (player, message) = register(user).expect("User not found");
             debug!("{} player loaded", player.username);
+            bot.send_message(msg.chat.id, message).await?
+        },
+        Command::NewGame => {
+            let chat = msg.chat.id;
+            let (game, message) = new_game(&chat).expect("Game not found");
+            debug!("{} game loaded", game.chat_id);
             bot.send_message(msg.chat.id, message).await?
         }
     };
